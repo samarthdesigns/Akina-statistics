86990a6c27295101193b8c660220b6ed1699322a
Samarth Nayyar <samarthnayyar123@gmail.com>
2020-04-08 00:04:28 +0530

    Added error message deep checks

--- js/index.js
+++ js/index.js
@@ -3,98 +3,130 @@ let req_count = null;
 
 getLandingData()
 
-function getLandingData(){
+function getLandingData() {
 
     fetch('https://stats.ayushpriya.tech/user_count ', {
-            method:'GET',
-            crossDomain:true,
-            headers:{
-                "Accept": "application/json"
-            },
-        })
+        method: 'GET',
+        crossDomain: true,
+        headers: {
+            "Accept": "application/json"
+        },
+    })
         .then((response) => response.json())
-        .then((result)=>{
+        .then((result) => {
             user_count = result;
             updateUserNumber();
         })
 
-        fetch('https://stats.ayushpriya.tech/req_count ', {
-            method:'GET',
-            crossDomain:true,
-            headers:{
-                "Accept": "application/json"
-            },
-        })
+    fetch('https://stats.ayushpriya.tech/req_count ', {
+        method: 'GET',
+        crossDomain: true,
+        headers: {
+            "Accept": "application/json"
+        },
+    })
         .then((response) => response.json())
-        .then((result)=>{
+        .then((result) => {
             req_count = result;
             updateRequestNumber();
         })
 
 }
 
-function updateUserNumber(){
+function updateUserNumber() {
     document.getElementById('user-number').innerHTML = user_count;
 }
 
-function updateRequestNumber(){
+function updateRequestNumber() {
     document.getElementById('request-number').innerHTML = req_count;
 }
 
-function checkEmail(data){
+function checkEmail(data) {
 
-    fetch('https://stats.ayushpriya.tech/user/' + data, {
-            method:'GET',
-            crossDomain:true,
-            headers:{
+    if (emailValidity == true) {
+        fetch('https://stats.ayushpriya.tech/user/' + data, {
+            method: 'GET',
+            crossDomain: true,
+            headers: {
                 "Accept": "application/json"
             },
         })
-        .then((response) => response.json())
-        .then((result)=>{
-            if(result === 'User does not exist')
-            {
+            .then((response) => response.json())
+            .then((result) => {
+                if (result === 'User does not exist') {
+                    document.getElementById('popup').classList.add('error');
+                    document.getElementById('popup').innerHTML = "User Does Not Exist";
+                    setTimeout(function () {
+                        document.getElementById('popup').classList.remove('error');
+                    }, 3000);
+                }
+                else if (result === 'Verified') {
+                    document.getElementById('popup').classList.add('success');
+                    document.getElementById('popup').innerHTML = "User Is Verified";
+                    setTimeout(function () {
+                        document.getElementById('popup').classList.remove('success');
+                    }, 3000);
+                }
+                else if (result === 'Unverified') {
+                    document.getElementById('popup').classList.add('error');
+                    document.getElementById('popup').innerHTML = "User Is Unverified";
+                    setTimeout(function () {
+                        document.getElementById('popup').classList.remove('error');
+                    }, 3000);
+                }
+                else {
+                    document.getElementById('popup').classList.add('error');
+                    document.getElementById('popup').innerHTML = "Some Issue On Our End";
+                    setTimeout(function () {
+                        document.getElementById('popup').classList.remove('error');
+                    }, 3000);
+                }
+            })
+            .catch(() => {
                 document.getElementById('popup').classList.add('error');
-                document.getElementById('popup').innerHTML = "User Does Not Exist";
+                document.getElementById('popup').innerHTML = "Check Email Field";
                 setTimeout(function () {
                     document.getElementById('popup').classList.remove('error');
                 }, 3000);
-            }
-            else if(result === 'Verified'){
-                document.getElementById('popup').classList.add('success');
-                document.getElementById('popup').innerHTML = "User Is Verified";
-                setTimeout(function () {
-                    document.getElementById('popup').classList.remove('success');
-                }, 3000);
-            }
-            else if(result === 'Unverified'){
-                document.getElementById('popup').classList.add('error');
-                document.getElementById('popup').innerHTML = "User Is Unverified";
-                setTimeout(function () {
-                    document.getElementById('popup').classList.remove('error');
-                }, 3000);
-            }
-            else{
-                document.getElementById('popup').classList.add('error');
-                document.getElementById('popup').innerHTML = "Some Issue On Our End";
-                setTimeout(function () {
-                    document.getElementById('popup').classList.remove('error');
-                }, 3000);
-            }
-        })
-        .catch((e)=>{
-            console.log(e)
-        })
+            })
+    }
+    else {
+        document.getElementById('popup').classList.add('error');
+        document.getElementById('popup').innerHTML = "Check Email Field";
+        setTimeout(function () {
+            document.getElementById('popup').classList.remove('error');
+        }, 3000);
+    }
 
 }
 
 
-document.getElementById('userCheckForm').addEventListener('submit',(e)=>{
+document.getElementById('userCheckForm').addEventListener('submit', (e) => {
 
     e.preventDefault();
 
     let user_email = document.getElementById('user-email').value;
 
     checkEmail(user_email);
+
+});
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+document.getElementById('email').addEventListener("input", function () {
+
+    emailValue = document.getElementById('email').value;
+
+    if (validateEmail(emailValue)) {
+        document.getElementById('email').classList.remove('text-field-false');
+        emailValidity = true;
+    }
+    else {
+        document.getElementById('email').classList.add('text-field-false')
+        emailValidity = false;
+    }
 
 });
